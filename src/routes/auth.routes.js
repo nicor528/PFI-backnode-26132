1fd2de0208ccf55ed0bc80bb43ac54d78cd2c939
Nicolas <nicolasriquelme528@gmail.com>
2026-07-01 19:37:59 -0300

    asd

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { login } from "../controladores/auth.controller.js"
+import { login } from "../controllers/auth.controller.js"
 
 const router = express.Router()
 
